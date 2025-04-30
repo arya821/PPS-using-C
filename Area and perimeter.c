4e2c0bd76b266675bc37bf21137840911fae669f
arya821 <avparte21@gmail.com>
2025-04-30 17:09:08 +0530

    Add files via upload

---
+++ Area and perimeter.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a,b,area,perimeter ;
+
+ printf(" Enter the length");
+ scanf("%d",&a);
+ printf(" Enter the width");
+ scanf("%d",&b);
+ area= a*b;
+ perimeter=2* (a+b);
+ printf ("area is %d and perimeter is%d",area ,perimeter);
+ return 0;
+ }
+ 
+  
+  
+
+
+  
+  
+  
+
