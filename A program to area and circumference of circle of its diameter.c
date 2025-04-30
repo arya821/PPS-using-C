eab707aaae9ddb0e2d4b99123e11e38c23df271a
arya821 <avparte21@gmail.com>
2025-04-30 17:27:21 +0530

    Add files via upload

---
+++ A program to area and circumference of circle of its diameter.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+ int main()
+ { 
+ float a,area ,circumference;
+ printf("Enter the diameter \t");
+ scanf ("%f",&a);
+ area= 3.14* (a/2);
+ circumference=2*(3.14)*(a/2);
+ printf (" area of the circle is%f\n",area);
+ printf (" circumference of the circle is% circumference %f\n");
+ return 0;
+ }
+ 
+ 
+  
+  
+
+
+  
+  
+  
+
