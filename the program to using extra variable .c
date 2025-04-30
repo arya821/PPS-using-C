afb550600cb81fdbf82971f7aee9401ca8ba7ba1
arya821 <avparte21@gmail.com>
2025-04-30 17:09:54 +0530

    Add files via upload

---
+++ the program to using extra variable .c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+ int main()
+ { 
+ int c,d,e;
+ printf(" Enter the value of c\t");
+ scanf("%d",&c);
+ printf(" Enter the value of d\t");
+ scanf("%d",&d);
+ printf("\n initially value of c is %d",c);
+ printf("\n initially value of d is %d",d);
+ e=c;
+ c=d;
+ d=e;
+ printf ("\n interchanged value of c is%d",c);
+ printf("\n  interchange value of d is %d",d);
+ return 0;
+ }
+ 
+ 
+  
+  
+
+
+  
+  
+  
+
