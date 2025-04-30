601c0175033e6b58c2b0588b35c9cd5b0ac86ff6
arya821 <avparte21@gmail.com>
2025-04-30 17:07:28 +0530

    Add files via upload

---
+++ Add of two number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,Add;
+
+ printf(" please Enter num1" );
+ scanf( "%d",&num1);
+ printf("please Enter num2");
+ scanf( "%d",&num2);
+ Add= num1+num2;
+ printf("* The Add of %d and %d",num1,num2,Add);
+ return 0;
+ }
+ 
+  
+  
+
+
+  
+  
+  
+
