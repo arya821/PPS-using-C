2212da2eae558de7884572c1ae910be38ea3010a
arya821 <avparte21@gmail.com>
2025-04-30 20:32:55 +0530

    Add files via upload

---
+++ A pass if marks greater than 40 and fail otherwise.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+ int main ()
+ { 
+   int marks;
+   printf( "Enter marks t");
+   scanf ("%d,&marks");
+   if ( marks>=40)
+ {
+   printf("PASS");
+ }
+   else
+ {
+ printf("FAIL");
+ }
+   return 0;
+ }
+ 
+ 
+  
+  
+
+
+  
+  
+  
+
