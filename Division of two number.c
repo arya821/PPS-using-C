eb92585ce30e328ac891aa52d9527bf2f4925a86
arya821 <avparte21@gmail.com>
2025-04-30 17:08:46 +0530

    Add files via upload

---
+++ Division of two number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,Div;
+
+ printf(" please Enter num1");
+ scanf( "%d",&num1);
+ printf("please Enter num2");
+ scanf( "%d",&num2);
+ Div= num1-num2;
+ printf("* The Div of %d and %d",num1,num2,Div);
+ return 0;
+ }
+ 
+  
+  
+
+
+  
+  
+  
+
