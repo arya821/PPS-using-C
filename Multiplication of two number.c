ec9844b0877be8cc62fa395a7ad1ec2c7b9b2d60
arya821 <avparte21@gmail.com>
2025-04-30 17:08:18 +0530

    Add files via upload

---
+++ Multiplication of two number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,multp;
+
+ printf(" please Enter num1");
+ scanf( "%d",&num1);
+ printf("please Enter num2");
+ scanf( "%d",&num2);
+ multp= num1-num2;
+ printf("* The multp of %d and %d",num1,num2,multp);
+ return 0;
+ }
+ 
+  
+  
+
+
+  
+  
+  
+
