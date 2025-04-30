0901492107d6d3c7da040df30e84107534d14a07
arya821 <avparte21@gmail.com>
2025-04-30 17:09:34 +0530

    Add files via upload

---
+++ A program to calculate gross salary.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+ int main()
+ { 
+ float bs,da,hr,gs;
+ printf(" Enter the bs\t");
+ scanf("%f ",&bs);
+ da=0.4*bs;
+ hr=0.2*bs;
+ gs=bs+da+hr;
+ printf("Gross salary of ramesh is %f",gs);
+ return 0;
+ }
+ 
+ 
+  
+  
+
+
+  
+  
+  
+
