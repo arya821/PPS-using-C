05a504b99db8c3fcb561b4f0a69ab8e5111c769a
arya821 <avparte21@gmail.com>
2025-04-30 18:04:42 +0530

    Add files via upload

---
+++ find the even and odd.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+ int main ()
+ { 
+   int num1; 
+   printf(" Enter the number");
+   scanf("%d",&num1);
+   if ( num1%2==0)
+ {
+   printf (" number is even");
+ }   
+  else
+ {
+   printf(" Number is odd");
+ }
+ return 0;
+ }
+ 
+ 
+  
+  
+
+
+  
+  
+  
+
