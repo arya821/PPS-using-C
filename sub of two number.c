54b91265e8405e573450c35f780521fe58c416be
arya821 <avparte21@gmail.com>
2025-04-30 17:07:56 +0530

    Add files via upload

---
+++ sub of two number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,sub;
+
+ printf(" please Enter num1");
+ scanf( "%d",&num1);
+ printf("please Enter num2");
+ scanf( "%d",&num2);
+ sub= num1-num2;
+ printf("* The sub of %d and %d",num1,num2,sub);
+ return 0;
+ }
+ 
+  
+  
+
+
+  
+  
+  
+
