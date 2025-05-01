453fc3840d038769a614d13a22d5858f4d2586f5
arya821 <avparte21@gmail.com>
2025-05-01 12:36:53 +0530

    Add files via upload

---
+++ A program to find largest number.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+ int main ()
+ { 
+ int num1,num2;
+ printf("Enter the first number");
+ scanf("%d",& num1);
+ 
+ printf("Enter the second number");
+ scanf("%d",&num2);
+ 
+ if (num1>num2)
+ {
+ printf("first number is greater %d",num1);
+ }
+ else if(num1=num2)
+ {
+ printf("both are equal");
+ }
+ else
+ {
+ printf("second number is greater %d",num2);
+ }
+ return 0;
+ }
+ 
+ 
+  
+  
+
+
+  
+  
+  
+
+    
