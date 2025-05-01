2d7a24141adb20cc1d9dfcb80496e1fc58533a5c
arya821 <avparte21@gmail.com>
2025-05-01 12:06:53 +0530

    Add files via upload

---
+++ A program to print student grade if marks..c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+ int main ()
+ { 
+ int marks;
+ printf("Enter marks \t:") ;
+ scanf("%d",&marks);
+ 
+ if(marks>=90)
+ {
+ printf("Excellent");
+ }
+ else if(marks>=80)
+ {
+ printf("Grade A");
+ }
+ else if(marks>=70)
+ {
+ printf("Grade B");
+ }
+ else if(marks>=60)
+ {
+ printf("Grade C");
+ }
+ else if(marks>=50)
+ {
+ printf("Grade D");
+ }
+ else
+ {
+  printf("Fail");
+ }
+ return 0;
+ }
+ 
+ 
+  
+  
+
+
+  
+  
+  
+
+    
