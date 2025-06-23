121f68420a66860c7479c6a55622d46a289a51e2
arya821 <avparte21@gmail.com>
2025-06-23 22:45:42 +0530

    Add files via upload

---
+++ To find first is 15 number of series.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+ int main ()
+ {
+ int i, pn=1,cn,nn;
+ for(i=1; i<=15; i++)
+ {
+ pn=cn;
+ cn=nn;
+ pn=pn+cn;
+ printf("%d \n",nn);
+ }
+ return 0;
+ }
+  
+
+
+  
+  
+  
+
+    
