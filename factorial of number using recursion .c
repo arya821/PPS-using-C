5ab9ebf0471438b1cb4b89cca4f90859723c710a
arya821 <avparte21@gmail.com>
2025-07-19 23:42:07 +0530

    Add files via upload

---
+++ factorial of number using recursion .c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+    int num , t=0 ,t1=1 ,t2 ,i;
+    printf("Enter the term of fibonacci series:");
+    scanf("%d",&num);
+    printf("Fibonacci series:");
+    for(i=0;i<num;i++)
+    {
+           if(i<=1)
+           {
+              t2 =i; 
+           }
+           else 
+           { 
+               t2 = t + t1;
+               t = t1;
+               t1 = t2;
+           }   
+           printf("%d\n" , t2);
+           
+    } 
+    return 0;
+}
+
+  
+  
+  
+
+    
